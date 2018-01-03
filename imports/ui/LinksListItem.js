a2ef5799dae0d6b476de187263e59fd3d5742b6e
Isaac <iavitia@outlook.com>
2018-01-03 03:20:59 -0700

    Clipboard setup

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Clipboard from 'clipboard';
 
 export default class LinksListItem extends React.Component {
+  componentDidMount() {
+    this.clipboard = new Clipboard(this.refs.copy);
+
+    this.clipboard.on('success', () => {
+      alert('It worked');
+    }).on('error', () => {
+      alert('Unable to copy. Please manually copy');
+    })
+  }
+  componentWillUnmount() {
+    this.clipboard.destroy();
+  }
   render() {
     return (
       <div>
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
+        <button ref="copy" data-clipboard-text={this.props.shortUrl} >Copy</button>
       </div>
     );
   }
