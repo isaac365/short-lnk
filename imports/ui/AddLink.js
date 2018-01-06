6a54f8938d3457287d3fe6a34499f9d27bbe3961
Isaac <iavitia@outlook.com>
2018-01-05 22:28:56 -0700

    Controlled checkbox

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -2,22 +2,41 @@ import React from 'react';
 import {Meteor} from 'meteor/meteor';
 
 export default class AddLink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url:''
+    };
+  }
+  
   onSubmit(e) {
-    const url = this.refs.url.value.trim();
+    const url = this.state.url
 
     e.preventDefault();
 
     if(url) {
-      Meteor.call('links.insert', url)
+      Meteor.call('links.insert', url, (err, res) => {
+        if (!err) {
+          this.setState({url: ''});
+        }
+      });
       this.refs.url.value = '';
     }
-	};
+  };
+  onChange(e) {
+    this.setState({
+      url: e.target.value
+    });
+  }
   render() {
     return (
       <div>
         <form onSubmit={this.onSubmit.bind(this)}>
-          <input type="text" ref="url" placeholder="URL"/>
-          <button>Add Link</button>
+          <input type="text"
+            placeholder="URL" 
+            value={this.state.url} 
+            onChange={this.onChange.bind(this)}/>
+            <button>Add Link</button>
         </form>
       </div>
     )
