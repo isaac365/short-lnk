14d01129ac7303758fbe646d434fb0b99c799cde
Isaac <iavitia@outlook.com>
2018-01-09 01:59:51 -0700

    Styling page

--- imports/ui/Links.js
+++ imports/ui/Links.js
@@ -9,9 +9,11 @@ export default () => {
 	return (
 		<div>
 			<PrivateHeader title="Your Links from Links"/>
-			<LinksListFilters/>
-			<AddLink/>
-			<LinksList/>
+			<div className="page-content" >
+				<LinksListFilters/>
+				<AddLink/>
+				<LinksList/>
+			</div>
 		</div>
 	);
 };
