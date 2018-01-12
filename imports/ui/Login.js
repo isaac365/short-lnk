0a555623ab6161b5749647884006c1e9d08f8ccd
Isaac <iavitia@outlook.com>
2018-01-12 04:55:53 -0700

    Update login

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -36,7 +36,7 @@ export default class Login extends React.Component {
               </form>
 
 
-              <Link to="/signup">Don't have an account?</Link>
+              <Link to="/signup">Need an account?</Link>
             </div>
           </div>
       );
